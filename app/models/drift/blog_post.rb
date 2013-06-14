2ba8c294d3a7f2d5bb80cfd476b87c0e08a2b53b
Jim Pruetting <jim@roboticmethod.com>
2013-06-14 12:03:54 -0500

    Move blog_post methods to base class.

--- app/models/drift/blog_post.rb
+++ app/models/drift/blog_post.rb
@@ -1,41 +1,5 @@
 module Drift
-  class BlogPost < Drift::StaticModel
-    self.source = 'db/static/blog_posts'
-
-    attr_accessor :date, :slug, :content, :title, :author, :version
-
-    def self.find_by_slug(slug)
-      file = self.all_files.detect do |f|
-        slug == parse_slug_from_filename(f)
-      end
-
-      find_by_file(file)
-    end
-
-    def initialize(args)
-      super
-      set_attributes_from_filename args[:file]
-    end
-
-    def content
-      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
-      markdown.render(@content)
-    end
-
-    private
-
-    def self.parse_date_from_filename(filename)
-      Date.parse filename.split(/[_|\.]/)[0]
-    end
-
-    def self.parse_slug_from_filename(filename)
-      filename.split(/[_|\.]/)[1]
-    end
-
-    def set_attributes_from_filename(filename)
-      self.date = self.class.parse_date_from_filename(filename)
-      self.slug = self.class.parse_slug_from_filename(filename)
-    end
+  class BlogPost < Drift::BlogPostBase
 
   end
 end
---
+++ app/models/drift/blog_post_base.rb
@@ -0,0 +1,51 @@
+module Drift
+  class BlogPostBase < Drift::StaticModel
+    class << self
+      attr_accessor :markup_ext
+
+      def source
+        @source || 'db/static/blog_posts'
+      end
+
+      def markup_ext
+        @markup_ext || 'md'
+      end
+
+      def find_by_slug(slug)
+        file = all_files.detect do |f|
+          slug == parse_slug_from_filename(f)
+        end
+
+        find_by_file(file)
+      end
+
+      def parse_datetime_from_filename(filename)
+        date = filename.split(/[_|\.]/)[0]
+        time = filename.split(/[_|\.]/)[1]
+        DateTime.parse "#{date} #{time[0..1]}:#{time[2..3]}:#{time[4..5]}"
+      end
+
+      def parse_slug_from_filename(filename)
+        filename.split(/[_|\.]/)[2]
+      end
+    end
+
+    attr_accessor :created_at, :slug, :content, :title, :author, :version
+
+    def initialize(args)
+      super
+      set_attributes_from_filename args[:file]
+    end
+
+    def content
+      GitHub::Markup.render(@file, @content)
+    end
+
+    private
+
+    def set_attributes_from_filename(filename)
+      self.created_at = self.class.parse_datetime_from_filename(filename)
+      self.slug = self.class.parse_slug_from_filename(filename)
+    end
+  end
+end
