474e1c011bc31f7bfe4c1a409a8ea307ca156661
Jim Pruetting <jim@roboticmethod.com>
2013-06-15 18:09:59 -0500

    Require blog_post model

--- lib/drift.rb
+++ lib/drift.rb
@@ -2,6 +2,7 @@ module Drift
   require "drift/version"
   require "drift/static_model"
   require "drift/blog_post_base"
+  require_relative '../app/models/drift/blog_post'
 
   require "github/markup"
 end
